313e9f72095807503a7be43ab846b9f5bb2967fa
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-19 18:44:53 +0200

    subir ejercicio hecho

---
+++ GabrielCortesT20Nuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GabrielCortesT20Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio añadir paquetes Nuget!");
+        }
+    }
+}
